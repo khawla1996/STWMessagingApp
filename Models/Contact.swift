6e279c7ecb4ad0cc39547cf2aae46c39557d1825
Khawla Ben Omar <37881780+khawla1996@users.noreply.github.com>
2025-11-01 17:10:56 +0100

    Update Contact.swift

--- Models/Contact.swift
+++ Models/Contact.swift
@@ -6,8 +6,20 @@
 //
 
 import Foundation
-struct Contact: Codable, Identifiable {
+struct Contact: Codable, Identifiable
+{
+    /// for contact id
     var id = UUID()
+    /// for contact name
     var name: String
+    // for contact phone number
     var phoneNumber: String
+    /// Optional profile image stored as binary data
+    var imageData: Data?
+    
+    /// Returns a UIImage if image data is available
+    var profileImage: UIImage? {
+        guard let imageData = imageData else { return nil }
+        return UIImage(data: imageData)
+    }
 }
