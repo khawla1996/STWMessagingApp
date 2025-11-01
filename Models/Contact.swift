e196a49bc564bc37dac598434835572fb7e6eb8e
Khawla Ben Omar <37881780+khawla1996@users.noreply.github.com>
2025-11-01 17:06:29 +0100

    Update Contact.swift

--- Models/Contact.swift
+++ Models/Contact.swift
@@ -1,1 +1,13 @@
+//
+//  Contact.swift
+//  STWMessagingApp
+//
+//  Created by khawla on 01/11/2025.
+//
 
+import Foundation
+struct Contact: Codable, Identifiable {
+    var id = UUID()
+    var name: String
+    var phoneNumber: String
+}
