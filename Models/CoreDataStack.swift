346c25d72ddbedb1dc669aaf91b37557d40e32a4
Khawla Ben Omar <37881780+khawla1996@users.noreply.github.com>
2025-11-04 20:05:48 +0100

    Update CoreDataStack.swift

--- Models/CoreDataStack.swift
+++ Models/CoreDataStack.swift
@@ -1,1 +1,28 @@
 
+import CoreData
+
+final class CoreDataStack {
+    static let shared = CoreDataStack()
+    let persistentContainer: NSPersistentContainer
+
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "STWMessagingApp")
+        persistentContainer.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Core Data failed to load: \(error)")
+            }
+        }
+    }
+
+    var context: NSManagedObjectContext { persistentContainer.viewContext }
+
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print("Save failed: \(error)")
+            }
+        }
+    }
+}
