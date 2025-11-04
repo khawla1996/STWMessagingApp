50137cd0ae8e178c076d405606d84efd1e3b8ff6
Khawla Ben Omar <37881780+khawla1996@users.noreply.github.com>
2025-11-04 20:02:33 +0100

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,1 +1,16 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("✅ STWMessagingApp launched")
+        return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        CoreDataStack.shared.saveContext()
+    }
+}
 
