7580ac2686eb0279f2917864dd9350c45a5cd926
Khawla Ben Omar <37881780+khawla1996@users.noreply.github.com>
2025-11-05 17:45:05 +0100

    Update POIListViewController.swift

--- Controllers/POIListViewController.swift
+++ Controllers/POIListViewController.swift
@@ -1,57 +1,81 @@
+//
+//  STWMessagingApp
+//
+//  Created by khawla 
+//
+
+
 import UIKit
 import CoreLocation
 import MapKit
 
+// Controller displaying a list of Points of Interest (POIs) near the user
 class POIListViewController: UITableViewController, CLLocationManagerDelegate {
 
-    var locationManager = CLLocationManager()
-    var pois: [POIItem] = []
+var locationManager = CLLocationManager()
+var pois: [POIItem] = []
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = NSLocalizedString("Points of Interest", comment: "")
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        loadPOIs()
-    }
+override func viewDidLoad() {
+    super.viewDidLoad()
+    title = NSLocalizedString("Points of Interest", comment: "")
+    locationManager.delegate = self
+    locationManager.requestWhenInUseAuthorization()
+    locationManager.startUpdatingLocation()
+     // Load static POI data
+    loadPOIs()
+}
+ // Load predefined POIs with their coordinates
+func loadPOIs() {
+    pois = [
+        POIItem(name: "Coffee Shop", latitude: 36.81, longitude: 10.18),
+        POIItem(name: "Mall", latitude: 36.83, longitude: 10.15),
+        POIItem(name: "Park", latitude: 36.84, longitude: 10.20)
+    ]
+    tableView.reloadData()
+}
+// Called whenever the user's location changes
 
-    func loadPOIs() {
-        pois = [
-            POIItem(name: "Coffee Shop", latitude: 36.81, longitude: 10.18),
-            POIItem(name: "Mall", latitude: 36.83, longitude: 10.15),
-            POIItem(name: "Park", latitude: 36.84, longitude: 10.20)
-        ]
-        tableView.reloadData()
-    }
+func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let current = locations.last else { return }
+            // Update each POI's distance from the user's current position
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let current = locations.last else { return }
-        for i in 0..<pois.count {
-            pois[i].updateDistance(from: current)
-        }
-        tableView.reloadData()
+    for i in 0..<pois.count {
+        pois[i].updateDistance(from: current)
     }
+            // Refresh the table to display updated distances
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pois.count
-    }
+    tableView.reloadData()
+}
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "poiCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "poiCell")
-        let poi = pois[indexPath.row]
-        cell.textLabel?.text = poi.name
-        if let dist = poi.distance {
-            cell.detailTextLabel?.text = "\(Int(dist)) m"
-        }
-        return cell
-    }
+// MARK: - Table View Data Source
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let poi = pois[indexPath.row]
-        let coord = CLLocationCoordinate2D(latitude: poi.latitude, longitude: poi.longitude)
-        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coord))
-        mapItem.name = poi.name
-        mapItem.openInMaps()
+// Number of POIs to display in the list
+override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return pois.count
+}
+
+    // Configure each table cell with POI name and distance
+
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "poiCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "poiCell")
+    let poi = pois[indexPath.row]
+    // Display POI name
+    cell.textLabel?.text = poi.name
+    // Display POI distance if available
+    if let dist = poi.distance {
+        cell.detailTextLabel?.text = "\(Int(dist)) m"
     }
+    return cell
+}
+
+ // When a POI is selected, open its location in Apple Maps
+override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let poi = pois[indexPath.row]
+    let coord = CLLocationCoordinate2D(latitude: poi.latitude, longitude: poi.longitude)
+            // Create a MapKit item and open in Maps app
+
+    let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coord))
+    mapItem.name = poi.name
+    mapItem.openInMaps()
+}
 }
