e53066b832525d81c9c3d3f178e80498e461529f
Khawla Ben Omar <37881780+khawla1996@users.noreply.github.com>
2025-11-05 17:48:07 +0100

    Update LocationManager.swift

--- Managers/LocationManager.swift
+++ Managers/LocationManager.swift
@@ -2,52 +2,64 @@ import Foundation
 import CoreLocation
 import UIKit
 
-/// Small wrapper around CLLocationManager to request one-shot location easily.
+/// A simple helper class to get the user's current location once (one-shot)
 public final class LocationManager: NSObject {
+    
+    // Shared instance (singleton) used throughout the app
     public static let shared = LocationManager()
+    
+    // CLLocationManager instance from CoreLocation
     private let manager = CLLocationManager()
+    
+    // Completion handler to return the location result
     private var completion: ((CLLocation?) -> Void)?
 
+    // Private initializer so only one instance can exist
     private override init() {
         super.init()
         manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.desiredAccuracy = kCLLocationAccuracyBest  // Use best accuracy available
     }
 
-    /// Request current location (one-shot). The completion is called with the last known location or nil.
+    /// Request the user's current location (called once)
+    /// The completion block returns the location or nil if failed
     public func requestLocation(completion: @escaping (CLLocation?) -> Void) {
         self.completion = completion
 
-        // Authorization
+        // Check the authorization status
         let status = CLLocationManager.authorizationStatus()
+        
+        // If permission not asked yet, request it
         if status == .notDetermined {
             manager.requestWhenInUseAuthorization()
         }
 
-        // If user denied, return nil
+        // If user denied or restricted access, return nil
         if status == .denied || status == .restricted {
             completion(nil)
             return
         }
 
+        // Request one-time location update
         manager.requestLocation()
     }
 }
 
+// MARK: - CLLocationManagerDelegate
 extension LocationManager: CLLocationManagerDelegate {
+    
+    // Called when location is successfully retrieved
     public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let loc = locations.last
-        completion?(loc)
-        completion = nil
+        let loc = locations.last   // Get the most recent location
+        completion?(loc)           // Return location via completion
+        completion = nil           // Clear completion to avoid multiple calls
     }
 
+    // Called if location update fails
     public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("LocationManager error:", error.localizedDescription)
-        completion?(nil)
+        completion?(nil)   // Return nil to indicate failure
         completion = nil
     }
 
-    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        // nothing special here; next requestLocation() will attempt to get location
-    }
 }
